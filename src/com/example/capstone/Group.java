929949cd5cfb33ddfbd41f9bece4c81a8b69eda5
kbrownj1 <kbrownj1@gmail.com>
2012-11-12 21:32:01 -0800

    Changes made on 2011.11.11 Sunday at miao's.  Minor changes to AndroidManifest to make SplashScreen main activity; comment out unused test methods in Group.java; Use alternate server at openfireserver.hopto.org.

--- src/com/example/capstone/Group.java
+++ src/com/example/capstone/Group.java
@@ -89,17 +89,17 @@ public class Group extends Activity{
 	 * Add mock data to database table user_info.
 	 * @param db	Reference to the database containing user_info table.
 	 */
-	private static void addMockUsers(SQLiteDatabase db) {
-		
-		addRowToDb(db, "person A", 37.3041, -121.8727, null);
-		
-		addRowToDb(db, "person B", 37.3000, -121.8700, null);
-		addRowToDb(db, "person C", 37.3100, -121.8800, null);
-		
-		addRowToDb(db, "Sam Artin", 37.0850, -121.6092, Constants.EMERGENCY);
-		
-		
-	}
+//	private static void addMockUsers(SQLiteDatabase db) {
+//		
+//		addRowToDb(db, "person A", 37.3041, -121.8727, null);
+//		
+//		addRowToDb(db, "person B", 37.3000, -121.8700, null);
+//		addRowToDb(db, "person C", 37.3100, -121.8800, null);
+//		
+//		addRowToDb(db, "Sam Artin", 37.0850, -121.6092, Constants.EMERGENCY);
+//		
+//		
+//	}
 	
 	/**
 	 * Add single row to user_info table
@@ -110,20 +110,20 @@ public class Group extends Activity{
 	 * @param snippet	Empty string unless EMERGENCY.
 	 * @param snippet TODO
 	 */
-	private static void addRowToDb(SQLiteDatabase db, String name, double lat, double lon, String snippet) {
-		ContentValues values = new ContentValues();
-		values.put("name", name);
-		values.put("lat", lat);
-		values.put("long", lon);
-		int inTroubleValue = Constants.EMERGENCY.equals(snippet) ? 1 : 0;
-		values.put("in_trouble", inTroubleValue);
-		
-		if(db.update("user_info", values, "name='" + name + "'", null) < 1 ) {
-			db.insert("user_info", "_id", values);
-			Log.d("CNL", "ERIK: DATABASE UPDATED, USER " + name + " " + (inTroubleValue != 0 ? Constants.EMERGENCY : "") + " ADDED");
-		}
-		else
-			Log.d("CNL", "ERIK: DATABASE UPDATED, USER " + name + " " + (inTroubleValue != 0 ? Constants.EMERGENCY : "") + " UPDATED");
-		
-	}
+//	private static void addRowToDb(SQLiteDatabase db, String name, double lat, double lon, String snippet) {
+//		ContentValues values = new ContentValues();
+//		values.put("name", name);
+//		values.put("lat", lat);
+//		values.put("long", lon);
+//		int inTroubleValue = Constants.EMERGENCY.equals(snippet) ? 1 : 0;
+//		values.put("in_trouble", inTroubleValue);
+//		
+//		if(db.update("user_info", values, "name='" + name + "'", null) < 1 ) {
+//			db.insert("user_info", "_id", values);
+//			Log.d("CNL", "ERIK: DATABASE UPDATED, USER " + name + " " + (inTroubleValue != 0 ? Constants.EMERGENCY : "") + " ADDED");
+//		}
+//		else
+//			Log.d("CNL", "ERIK: DATABASE UPDATED, USER " + name + " " + (inTroubleValue != 0 ? Constants.EMERGENCY : "") + " UPDATED");
+//		
+//	}
 }
